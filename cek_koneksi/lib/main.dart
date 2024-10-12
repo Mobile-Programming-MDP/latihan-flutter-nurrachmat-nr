f92f7763ee891a97392c392f8403c601a156f73a
Yohannes Masterous <rachmat.nur91@gmail.com>
2024-10-12 12:42:49 +0700

    Latihan Widget

--- cek_koneksi/lib/main.dart
+++ cek_koneksi/lib/main.dart
@@ -54,6 +54,8 @@ class _HomeState extends State<Home> {
     });
   }
 
+
+
   
 
   @override
---
+++ latihan_widget/lib/tugaswidget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GaleriFoto extends StatelessWidget {
+  const GaleriFoto({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
